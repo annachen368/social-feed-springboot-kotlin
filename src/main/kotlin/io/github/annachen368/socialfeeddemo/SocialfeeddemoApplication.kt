b0358d440559824cd88a1324bf7159863f577f21
anna <annachen368@gmail.com>
2025-07-21 16:30:30 -0700

    Initial commit

---
+++ src/main/kotlin/io/github/annachen368/socialfeeddemo/SocialfeeddemoApplication.kt
@@ -0,0 +1,11 @@
+package io.github.annachen368.socialfeeddemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialfeeddemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SocialfeeddemoApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/annachen368/socialfeeddemo/SocialfeeddemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.annachen368.socialfeeddemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocialfeeddemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
