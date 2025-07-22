e83d32a07d633c2c61e357f7dad0f08584ba6282
anna <annachen368@gmail.com>
2025-07-21 17:28:56 -0700

    Add getPostById method to PostController for retrieving a single post by ID

--- src/main/kotlin/io/github/annachen368/socialfeeddemo/controller/PostController.kt
+++ src/main/kotlin/io/github/annachen368/socialfeeddemo/controller/PostController.kt
@@ -3,6 +3,7 @@ package io.github.annachen368.socialfeeddemo.controller
 import io.github.annachen368.socialfeeddemo.dto.CreatePostRequestDto
 import io.github.annachen368.socialfeeddemo.model.Post
 import io.github.annachen368.socialfeeddemo.repository.PostRepository
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -22,4 +23,10 @@ class PostController(private val postRepo: PostRepository) {
         )
         return postRepo.save(post)
     }
+
+    // Read single post
+    @GetMapping("/{id}")
+    fun getPostById(@PathVariable id: Long): ResponseEntity<Post> =
+        postRepo.findById(id).map { ResponseEntity.ok(it) }
+            .orElse(ResponseEntity.notFound().build())
 }
